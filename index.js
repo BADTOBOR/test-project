9c86d61b34da1b110b64b6088262d55796415d96
BadRobot <doctore.ravani@gmail.com>
2023-11-22 18:01:49 +0330

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("hello there....");
-alert("himan");
+function sum(n) {
+    let result = n;
+
+    result = result - Math.floor(result / 100) * 100;
+
+    result = result - Math.floor(result / 50) * 50;
+
+    result = result - Math.floor(result / 20) * 20;
+
+    result = result - Math.floor(result / 10) * 10;
+
+
+    return result;
+}
+
+console.log(sum(489))
+
+
+
+
+
+
+
+
+
